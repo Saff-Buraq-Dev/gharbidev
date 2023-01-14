802e37577ab0c16b631a26ad9213ee9ee0f8fa69
Saff-Buraq-Dev <Saff-Buraq-Dev@users.noreply.github.com>
2023-01-14 16:28:08 +0000

    deploy: 167a64046dca0e118bf409d4435392a106a48f49

--- assets/use-tick.628b4262.js
+++
@@ -1,1 +0,0 @@
-import{c as i}from"./vm.58da4068.js";import{Y as r,l as s,n as c,g as a}from"./index.d1b41836.js";function f(){let e;const n=a();function t(){e=void 0}return r(t),s(t),{removeTick:t,registerTick(o){e=o,c(()=>{e===o&&(i(n)===!1&&e(),e=void 0)})}}}export{f as u};
